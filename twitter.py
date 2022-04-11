682e207911fb76f93cdc0fee93261947fad8a0a0
KillianG <killian.gardahaut@scality.com>
2022-04-11 18:09:59 +0200

    initial commit

---
+++ twitter.py
@@ -0,0 +1,43 @@
+import tweepy
+import os
+import pprint
+
+CONSUMER_API_KEY = os.environ.get('CONSUMER_API_KEY')
+CONSUMER_API_SECRET = os.environ.get('CONSUMER_API_SECRET')
+ACCESS_API_KEY = os.environ.get('ACCESS_API_KEY')
+ACCESS_API_SECRET = os.environ.get('ACCESS_API_SECRET')
+
+auth = tweepy.OAuthHandler(CONSUMER_API_KEY, CONSUMER_API_SECRET)
+auth.set_access_token(ACCESS_API_KEY, ACCESS_API_SECRET)
+pprint = pprint.PrettyPrinter()
+
+list_low_price = []
+last_sold = ""
+
+def send_t(message):
+    if CONSUMER_API_SECRET and CONSUMER_API_KEY and ACCESS_API_SECRET and ACCESS_API_KEY:
+        auth = tweepy.OAuthHandler(CONSUMER_API_KEY, CONSUMER_API_SECRET)
+        auth.set_access_token(ACCESS_API_KEY, ACCESS_API_SECRET)
+        api = tweepy.API(auth)
+        api.update_status(message)
+    else:
+        print('message not sent')
+    print(f'--------------- message messaged send: {message}---------------')
+
+
+def send_message(listingId, initialCost, ownerPercent, borrowerPercent, otherPercent, gotchiBRS, gotchiName, timeLend):
+    global  list_low_price
+    global last_sold
+    if gotchiName == "":
+        gotchiName = "Unnamed"
+    message = f"""
+Gotchi "{gotchiName}" with BRS at {gotchiBRS} is available for lending !
+
+Initial cost: {initialCost:.3f} $GHST for {timeLend} HRS
+
+Details: owner takes {ownerPercent}%, borrower takes {borrowerPercent}%, and other takes {otherPercent}%
+ 
+👻 https://app.aavegotchi.com/lending/{listingId} 👻
+#AaveGotchi #Polygon
+"""
+    send_t(message)
